fae2143a09678fbc8c894320a7f1615862a84a8e
A-mont <a.mont.24.777@gmail.com>
2025-08-27 20:08:16 -0600

    feat(ui): refresh header, add network badge and wallet button

--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -6,8 +6,10 @@ import { Logo } from './logo';
 function Header() {
   return (
     <header className={styles.header}>
-      <Logo />
-      <Wallet theme='gear' />
+      <a href="#" className="-m-1.5 p-1.5 text-green-400 text-xl font-semibold">
+        VaraNames
+      </a>
+      <Wallet theme="gear" />
     </header>
   );
 }
